b0f28f2c0127961d4d521639a40c08f663af4e4e
pesterie <pesterie@gmail.com>
2012-05-17 12:22:27 -0500

    Module update for cmake

--- sdk/cmake/nt2.hpx.sdk.dependencies.cmake
+++ sdk/cmake/nt2.hpx.sdk.dependencies.cmake
@@ -8,14 +8,17 @@
 ################################################################################
 
 #This is a temporary fix : TODO extend support to other platforms.
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "/usr/local/share/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/Modules")
 
-include(FindHPX)
+find_package(PkgConfig)
+pkg_check_modules(NT2_HPX QUIET hpx_application)
 
-if(NOT HPX_VERSION)
+if(NOT NT2_HPX_FOUND)
   set(NT2_HPX_DEPENDENCIES_FOUND 0)
 endif()
 
-set(NT2_HPX_COMPILE_FLAGS ${HPX_FLAGS} CACHE INTERNAL "" FORCE)
+set(NT2_HPX_DEPENDENCIES_INCLUDE_DIR ${NT2_HPX_INCLUDE_DIRS})
+set(NT2_HPX_DEPENDENCIES_LIBRARIES ${NT2_HPX_LIBRARIES} "libhpx_component_dataflow.so libhpx_component_iostreams.so libhpx_serialization.so libhpx.so")
+set(NT2_HPX_DEPENDENCIES_COMPILE_FLAGS ${NT2_HPX_CFLAGS})
+set(NT2_HPX_DEPENDENCIES_LINK_FLAGS ${NT2_HPX_LDFLAGS})
 
 set(NT2_HPX_DEPENDENCIES_EXTRA sdk core unit)
