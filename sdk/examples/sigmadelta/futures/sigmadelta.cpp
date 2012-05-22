721ee03b9d89a74ebfb48ff931da1ec26328e946
pesterie <pesterie@gmail.com>
2012-05-22 16:49:28 -0500

    First step to use nt2::table

--- sdk/examples/sigmadelta/futures/sigmadelta.cpp
+++ sdk/examples/sigmadelta/futures/sigmadelta.cpp
@@ -13,55 +13,23 @@
 #include <iostream>
 #include <boost/cstdint.hpp>
 #include <boost/format.hpp>
-#include "images.hpp"
 #include "sigmadelta_test.hpp"
-#include "sigmadelta.hpp"
-#include "pgm.hpp"
+
+typedef nt2::test::get_sigmadelta_action<boost::uint8_t>::type sigmadelta_action;
 
 HPX_REGISTER_PLAIN_ACTION(sigmadelta_action);
 
 int hpx_main(boost::program_options::variables_map& vm)
 {   
-    Sigmadelta_test my_test;    
-    
-   // Recopie des parametres initiaux
-    std::size_t const h(my_test.h), w(my_test.w), Ni(my_test.Ni);
-    Images* im(my_test.im);
-    
-   // Demarrage chronomètre
-    hpx::util::high_resolution_timer t;
-    
-   // Lancement du Benchmark 
-    std::cout << "Algorithme sigmadelta\n";
-    nt2::unit::benchmark_result<nt2::details::cycles_t> dv;
-    nt2::unit::perform_benchmark(my_test,10., dv);
-    std::cout << "sigmadelta : " << dv.median/(double)(h*w*Ni)<< " cpe\n";
-
-   // Fin du chronomètre
-    char const* fmt = "Elapsed time: %1% [s]\n";
-    std::cout << (boost::format(fmt) % t.elapsed());
-
-   // Affichage des images fond 
-    for (std::size_t n=0; n<Ni; n++)
-    { 	  
-      std::ostringstream nom_fichier;	
-      nom_fichier<<"./frame_fond/fond."<<std::setfill('0')<<std::setw(3)<<n<<".pgm";      
-            
-      Pgm Fond(w,h,im->Fond[n].data.begin());
-      Fond.write(nom_fichier.str()); 	  
-    }
- 
-  // Affichage des images estimee 
-    for (std::size_t n=0; n<Ni; n++)
-    {   
-      std::ostringstream nom_fichier;	
-      nom_fichier<<"./frame_estime/estime."<<std::setfill('0')<<std::setw(3)<<n<<".pgm";      
-      
-      Pgm Estimee(w,h,im->Estimee[n].data.begin());
-      Estimee.write(nom_fichier.str());   
-    }
-     
-    return hpx::finalize(); // Handles HPX shutdown
+  nt2::test::sigmadelta_test<boost::uint8_t> my_test;    
+  hpx::util::high_resolution_timer t;
+  std::cout << "Sigma-Delta Algorithm\n";
+  nt2::unit::benchmark_result<nt2::details::cycles_t> dv;
+  nt2::unit::perform_benchmark(my_test,10., dv);
+  std::cout << "sigmadelta : " << dv.median/(double)(my_test.h*my_test.w*my_test.Ni)<< " cpe\n";
+  char const* fmt = "Elapsed time: %1% [s]\n";
+  std::cout << (boost::format(fmt) % t.elapsed());
+  return hpx::finalize(); // Handles HPX shutdown
 }
 
 
--- sdk/examples/sigmadelta/futures/sigmadelta_test.hpp
+++ sdk/examples/sigmadelta/futures/sigmadelta_test.hpp
@@ -9,30 +9,183 @@
 #ifndef NT2_SDK_EXAMPLES_SIGMADELTA_FUTURES_SIGMADELTA_TEST_HPP
 #define NT2_SDK_EXAMPLES_SIGMADELTA_FUTURES_SIGMADELTA_TEST_HPP
 
-#include "sigmadelta.hpp"
+#include <hpx/hpx.hpp>
+#include <hpx/config.hpp>
+#include <hpx/hpx_init.hpp>
+#include <hpx/lcos/future.hpp>
+#include <hpx/include/components.hpp>
+#include <hpx/include/iostreams.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/format.hpp>
+#include <boost/ref.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <nt2/table.hpp>
+#include <nt2/include/functions/of_size.hpp>
+#include <nt2/include/functions/repnum.hpp>
+#include <vector>
+#include <algorithm>
 
-class Sigmadelta_test
+namespace nt2{ namespace test 
 {
-public:  
-   std::size_t const Ni;	// Nombre d'images
-   std::size_t const h;		// Hauteur
-   std::size_t const w; 	// Largeur
-   std::size_t const bloc_i;    // Nombre de blocs
-   std::size_t const bloc_j;    // Nombre de blocs
+  
+  
+  template<class T>
+  class Images
+  {
+  public: 
 
+    typedef T scalar_type;
+    typedef container::table<scalar_type> table_;
 
-   std::vector<sigmadelta_future> promesses;      // Vecteur de futures
-   Images *im;	// Images original, fond, estimee
-                // et Matrices Moyennes
-   
-   // Constructeur par defaut
-    Sigmadelta_test():
-    Ni(181), h(1080), w(1920), bloc_i(180), bloc_j(960)
+    BOOST_SERIALIZATION_SPLIT_MEMBER()
+    
+    template<class Archive>
+    void save(Archive & ar, const unsigned int version) const
     {
-      im = initialisation(Ni, h, w, bloc_i, bloc_j);      
+      ar & I;
+      ar & Fond;
+      ar & Estimee;
+      ar & h;
+      ar & w;
+      ar & Ni;
+      ar & bloc_i;
+      ar & bloc_j;
+      ar & M;
+      ar & V;
+    }
+    
+    template<class Archive>
+    void load(Archive & ar, const unsigned int version)
+    {
+      ar & I;
+      ar & Fond;
+      ar & Estimee;
+      ar & h;
+      ar & w;
+      ar & Ni;
+      ar & bloc_i;
+      ar & bloc_j;
+      ar & M;
+      ar & V;
+    } 
+    
+    Images(std::size_t h_=1, std::size_t w_=1, std::size_t Ni_=1, std::size_t bi=1, std::size_t bj=1)
+      : Fond(Ni_, table_(nt2::of_size(h_,w_))), 
+        h(h_), w(w_), Ni(Ni_), bloc_i(bi), bloc_j(bj),
+        M(h_,w_),V(h_,w_,1)
+    {
+      std::fill(Estimee.begin(), Estimee.end(), nt2::repnum(255, nt2::of_size(h_, w_))); 
     };
     
-    // Operation
+    std::vector<table_> I, Fond, Estimee;   // Image vector
+    std::size_t h, w, Ni, bloc_i, bloc_j;   
+    table_ M, V;	                    // Temporary images
+  };
+
+  template<class T>
+  int sigmadelta(std::size_t n, std::size_t ii, std::size_t jj, Images<T>* im);
+
+  // Sigma-Delta action constructor
+  template<class T>
+  struct get_sigmadelta_action
+  {
+    typedef hpx::actions::plain_result_action4<
+      int,                                 // result type
+      std::size_t,                         // argument
+      std::size_t,                         // argument
+      std::size_t,                         // argument
+      Images<T>*,                          // argument
+      sigmadelta                           // wrapped function
+      > type;
+  }; 
+
+  // Sigma-Delta future constructor
+  template<class T>
+  struct get_sigmadelta_future
+  {
+    // Return type embedded in a future
+    typedef hpx::lcos::future<T> type;
+  };
+
+  // Sigma-Delta Function
+  template<class T>
+  int sigmadelta(std::size_t n, std::size_t ii, std::size_t jj, Images<T>* im)
+  {
+    std::size_t const N(3);
+    std::size_t const & size_i(im->bloc_i);
+    std::size_t const & size_j(im->bloc_j);
+    
+    // First frame processing
+    if (n < 1)
+    {       
+      for(std::size_t i=0; i<size_i; i++) 
+	for(std::size_t j=0; j<size_j; j++)
+	{
+	  boost::uint8_t const & I(im->I[0](i+ii,j+jj));
+	  im->M(i+ii,j+jj) = I; 
+	  im->Fond[0](i+ii,j+jj) = I;
+	}
+      
+      return 0;
+    }
+    
+    // This future waits for the results of the previous images.
+    typename get_sigmadelta_future<int>::type  res = 
+      hpx::lcos::async<typename get_sigmadelta_action<T>::type>(hpx::find_here(), n-1, ii, jj, im);
+    
+    res.get();
+
+    // Nth image processing
+    for(std::size_t i=0; i<size_i; i++)  
+      for(std::size_t j=0; j<size_j; j++)
+      {   	  
+	boost::uint8_t const & I(im->I[n](i+ii,j+jj));
+	boost::uint8_t d;        
+	boost::uint8_t & mt = im->M(i+ii,j+jj);
+	boost::uint8_t & vt = im->V(i+ii,j+jj);
+	  
+	if (mt < I) 
+	  mt = mt + 1; 
+	else if (mt > I) 
+	  mt = mt - 1;
+	  
+	im->Fond[n](i+ii,j+jj) = mt;  
+	  
+	if (mt < I)
+	  d = I - mt;	      
+	else
+	  d = mt - I;
+	
+	if (d != 0) 
+	{ 
+	  if (vt < N*d) 
+	    vt = vt + 1;		
+	  else if (vt > N*d)  	 
+	    vt =  vt - 1;
+	}
+	
+	if (d > vt)  
+	  im->Estimee[n](i+ii,j+jj) = 0; 
+      }
+    
+    return 0;
+  }
+
+  template<class T> 
+  struct sigmadelta_test
+  {
+    typedef T scalar_type;
+    typedef Images<scalar_type> image_vector_;
+    typedef std::vector<get_sigmadelta_future<int>::type> promesses_;
+    typedef typename get_sigmadelta_action<scalar_type>::type sigmadelta_action;
+    
+
+    sigmadelta_test():
+      Ni(181), h(1080), w(1920), bloc_i(180), bloc_j(960)
+    {
+      im = initialisation(Ni, h, w, bloc_i, bloc_j);      
+    }
+
     void operator()() 
     {  
       // Création des futures pour la dernière image
@@ -45,6 +198,33 @@ public:
       (hpx::lcos::async<sigmadelta_action>(hpx::find_here(), Ni-1, ii, jj, im));          
        hpx::lcos::wait(promesses);      
     };
-};
 
-#endif // SIGMADELTA_TEST_H
+    image_vector_*           im;          
+    promesses_        promesses;
+    std::size_t const Ni;	// image number
+    std::size_t const h;	// height
+    std::size_t const w; 	// width
+    std::size_t const bloc_i;   // block parameters
+    std::size_t const bloc_j;
+
+  protected :
+    image_vector_* initialisation(std::size_t Ni, std::size_t h, std::size_t w, 
+                            std::size_t bloc_i, std::size_t bloc_j)
+    {    
+      image_vector_* im_ptr = new image_vector_(h, w, Ni, bloc_i, bloc_j);
+      
+      for (std::size_t n=0; n<Ni; n++)
+      { 
+	std::ostringstream nom_fichier;	
+	nom_fichier<<"./frame/frame."<<std::setfill('0')<<std::setw(3)<<n<<".pgm";
+	// Pgm mon_image(nom_fichier.str()); 
+	// im_ptr->I.push_back(table_(h,w,mon_image.pixel));
+      }
+      
+      return im_ptr;
+    }
+  };
+
+} }
+
+#endif 
