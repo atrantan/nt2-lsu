49fc7acb8a1beff68a68bef304cf23f998151964
pesterie <pesterie@gmail.com>
2012-06-12 17:21:14 -0500

    Update serialization unit

--- core/unit/serialization.cpp
+++ core/unit/serialization.cpp
@@ -14,12 +14,17 @@
 #include <nt2/core/utility/of_size.hpp>
 #include <nt2/include/functions/repnum.hpp>
 #include <nt2/include/functions/numel.hpp>
+#include <nt2/include/functions/plus.hpp>
+#include <nt2/include/functions/multiplies.hpp>
+#include <nt2/core/container/dsl.hpp>
 
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/iostreams/stream_buffer.hpp> 
 #include <boost/iostreams/stream.hpp> 
 #include <boost/iostreams/device/back_inserter.hpp> 
+#include <boost/proto/debug.hpp>
+#include <boost/proto/make_expr.hpp>
 
 #include <vector>
 #include <iostream>
@@ -29,9 +34,9 @@
 #include <nt2/sdk/unit/tests/basic.hpp>
 
 //==============================================================================
-// Test for save table in an archive
+// Test for save and load table in an archive
 //==============================================================================
-NT2_TEST_CASE_TPL( serialization_save, (double))//NT2_TYPES)
+NT2_TEST_CASE_TPL( serialization_table, NT2_TYPES)
 {
   using boost::archive::binary_oarchive;
   using boost::archive::binary_iarchive;
@@ -61,6 +66,77 @@ NT2_TEST_CASE_TPL( serialization_save, (double))//NT2_TYPES)
     binary_iarchive ia(input_stream); 
     ia >> in;
   }
+
   for(std::size_t i = 0; i < nt2::numel(out); ++i)
     NT2_TEST_EQUAL(out(i), in(i));
+}
+
+//==============================================================================
+//Test for save and load expression in an archive
+//==============================================================================
+NT2_TEST_CASE_TPL( serialization_expression, (double))//NT2_TYPES)
+{
+  using boost::archive::binary_oarchive;
+  using boost::archive::binary_iarchive;
+  using boost::iostreams::stream;
+  using boost::iostreams::back_insert_device;
+  using boost::iostreams::basic_array_source;
+  using nt2::table;
+  using nt2::of_size;
+  
+  typedef std::vector<char> buffer_type;
+
+  buffer_type buffer;
+
+  table<T> aout(of_size(16));
+  table<T> bout(of_size(16));
+  table<T> cout(of_size(16));
+
+  aout = nt2::repnum(T(12), of_size(16));
+  bout = nt2::repnum(T(3.5), of_size(16));
+  cout = nt2::repnum(T(4.6), of_size(16));
+
+  typedef typename boost::proto::result_of::
+  make_expr< nt2::tag::plus_
+           , nt2::container::domain
+           , typename boost::proto::result_of::
+             make_expr< nt2::tag::multiplies_
+                      , nt2::container::domain
+                      , table<T> const&
+                      , table<T> const&
+                      >::type
+           , table<T> const&
+           >::type expr_type;
+
+  expr_type expr_out  = boost::proto::
+                        make_expr< nt2::tag::plus_
+                                 , nt2::container::domain
+                                 >( boost::cref( boost::proto::
+                                                 make_expr< nt2::tag::multiplies_
+                                                          , nt2::container::domain
+                                                          >( boost::cref(aout)
+                                                           , boost::cref(bout)))
+                                  , boost::cref(cout));
+
+  boost::proto::display_expr(expr_out);
+
+  expr_type expr_in();
+
+  stream< back_insert_device<buffer_type> > output_stream(buffer);
+  
+  {// save  
+    binary_oarchive oa(output_stream);
+    oa << expr_out;
+  }
+  
+  // basic_array_source<char> source(&buffer[0],buffer.size()); 
+  // stream< basic_array_source<char> > input_stream(source); 
+
+  // {// load  
+  //   binary_iarchive ia(input_stream); 
+  //   ia >> aout*bout+cout;
+  // }
+
+  // for(std::size_t i = 0; i < nt2::numel(out); ++i)
+  //   NT2_TEST_EQUAL(out(i), in(i));
 }
