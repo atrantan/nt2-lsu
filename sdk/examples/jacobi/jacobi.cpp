89f12c05742292a80f9550641a8ac080cf385c1f
pesterie <pesterie@gmail.com>
2012-06-13 14:27:15 -0500

    scalar version

--- sdk/examples/jacobi/jacobi.cpp
+++ sdk/examples/jacobi/jacobi.cpp
@@ -21,22 +21,17 @@
 
 using namespace nt2;
 
-namespace nt2{ namespace log 
-{
-  template<class T, class S, class Dummy>
-  void print_table( table<T,S,Dummy> const& t
-                  , std::size_t      const& h
-                  , std::size_t      const& w);
-} }
-
 namespace nt2{ namespace example 
 {
+  //============================================================================
+  // Simd version
+  //===========================================================================
   template<class T>
-  struct diffusion
+  struct diffusion_simd
   {
     typedef T s_t;
     
-    diffusion(std::size_t const& s, std::size_t const& iter)
+    diffusion_simd(std::size_t const& s, std::size_t const& iter)
       : size_(s), iterations_(iter)
     {
       new_.resize(of_size(size_,size_));
@@ -45,18 +40,19 @@ namespace nt2{ namespace example
       // Initialization of the first matrix
       for(std::size_t i = 1; i<=size_; ++i)
         for(std::size_t j = 1; j<=size_; ++j)
-          old_(i,j) = s_t(i+j); 
+          old_(i,j) = s_t(1); 
       
 #ifdef NT2_JACOBI_LOGGING
-      nt2::log::print_table(old_, size_, size_);
+      std::cout << old_ << std::endl;
 #endif
     }
  
+    BOOST_FORCEINLINE
     void operator()()
     {
       // Computation of the equation
       for(std::size_t i = 0; i<iterations_; ++i)
-      {
+      {   
         new_(_(begin_+1,end_-1),_(begin_+1,end_-1)) 
           = ( old_(_(begin_,end_-2),_(begin_+1,end_-1))
             + old_(_(begin_+2,end_),_(begin_+1,end_-1))
@@ -65,7 +61,7 @@ namespace nt2{ namespace example
             )/s_t(4);
         
 #ifdef NT2_JACOBI_LOGGING
-        nt2::log::print_table(new_, size_, size_);
+        std::cout << new_ << std::endl;
 #endif
         
         boost::swap(new_,old_);
@@ -74,8 +70,62 @@ namespace nt2{ namespace example
    
     std::size_t size_;
     std::size_t iterations_;
-    table<s_t> new_;
-    table<s_t> old_;
+    table<s_t,_2D> new_;
+    table<s_t,_2D> old_;
+  };
+
+  //============================================================================
+  // Scalar version
+  //===========================================================================
+  template<class T>
+  struct diffusion_scalar
+  {
+    typedef T s_t;
+    
+    diffusion_scalar(std::size_t const& s, std::size_t const& iter)
+      : size_(s), iterations_(iter)
+    {
+      new_.resize(of_size(size_,size_));
+      old_.resize(of_size(size_,size_));
+      
+      // Initialization of the first matrix
+      for(std::size_t i = 1; i<=size_; ++i)
+        for(std::size_t j = 1; j<=size_; ++j)
+          old_(i,j) = s_t(1); 
+      
+#ifdef NT2_JACOBI_LOGGING
+      std::cout << old_ << std::endl;
+#endif
+    }
+ 
+    BOOST_FORCEINLINE
+    void operator()()
+    {
+      // Computation of the equation
+      for(std::size_t i = 0; i<iterations_; ++i)
+      {
+        for(std::size_t i = 2; i<size_; ++i)
+          for(std::size_t j = 2; j<size_; ++j)
+          {  
+            new_(i,j) 
+              = ( old_(i+1,j)
+                + old_(i-1,j)
+                + old_(i,j+1)
+                + old_(i,j-1)
+                )/s_t(4);
+          }
+#ifdef NT2_JACOBI_LOGGING
+        std::cout << new_ << std::endl;
+#endif
+        
+        boost::swap(new_,old_);
+      }
+    }
+   
+    std::size_t size_;
+    std::size_t iterations_;
+    table<s_t,_2D> new_;
+    table<s_t,_2D> old_;
   };
 
 } }
@@ -91,30 +141,13 @@ int main(int argc, char* argv[])
   typedef float s_t;
 
   nt2::unit::benchmark_result<nt2::details::cycles_t> dv;
-  nt2::unit::perform_benchmark(nt2::example::diffusion<s_t>(size, iterations), 5., dv);
-  std::cout << "Diffusion Equation :\n\t";
-  std::cout << std::scientific << dv.median/(double)(size*size*iterations) << "\n";
+  nt2::unit::perform_benchmark(nt2::example::diffusion_simd<s_t>(size, iterations), 5., dv);
+  std::cout << "Diffusion Equation Simd :\n\t";
+  std::cout << dv.median/(double)(size*size*iterations) << "\n";
+
+  nt2::unit::perform_benchmark(nt2::example::diffusion_scalar<s_t>(size, iterations), 5., dv);
+  std::cout << "Diffusion Equation Scalar :\n\t";
+  std::cout << dv.median/(double)(size*size*iterations) << "\n";
 
   return 0;
 }
-
-// Logging facility
-namespace nt2{ namespace log 
-{
-  template<class T, class S, class Dummy>
-  void print_table( table<T,S,Dummy> const& t
-                  , std::size_t      const& h
-                  , std::size_t      const& w)
-  {
-    std::cout << "==================================\n";
-    for(std::size_t i = 1; i<=h; ++i)
-    {
-      for(std::size_t j = 1; j<=w; ++j)
-      {
-        std::cout << t(i,j) << " ";
-      }
-      std::cout << "\n";
-    }
-    std::cout << "==================================\n";
-  }
-} }
