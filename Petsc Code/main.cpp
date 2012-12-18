40136372867a0c1683a72d388a78e4e5be430569
antoine <antoine@MacBook-Pro-de-antoine.local>
2012-12-18 20:56:20 +0100

    Add Petsc GMRES Code

---
+++ Petsc Code/main.cpp
@@ -0,0 +1,72 @@
+
+/* Program usage:  mpiexec -n <procs> petsc_gemres [-help] [all PETSc options] */ 
+
+static char help[] = "Solves a linear system in parallel with KSP.\n\
+Input parameters include:\n\
+  -random_exact_sol : use a random exact solution vector\n\
+  -view_exact_sol   : write exact solution vector to stdout\n\
+  -N <mesh_size>       : number of mesh points in larger\n\
+  -maxits :maximum number of iterations";
+
+/*T
+   Concepts: KSP^basic parallel example;
+   Processors: n
+T*/
+
+/* 
+  Include "petscksp.h" so that we can use KSP solvers.  Note that this file
+  automatically includes:
+     petscsys.h       - base PETSc routines   petscvec.h - vectors
+     petscmat.h - matrices
+     petscis.h     - index sets            petscksp.h - Krylov subspace methods
+     petscviewer.h - viewers               petscpc.h  - preconditioners
+*/
+#include <iostream>
+
+#include "petscksp.h"
+#include "petsc_gmres_test.h"
+#include "benchmark.hpp"
+
+using namespace details;
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args)
+{   
+    int nprocs;
+  
+    PetscInitialize(&argc,&args,(char *)0,help);    
+    
+    MPI_Comm_size(PETSC_COMM_WORLD, &nprocs);
+    
+    petsc_gmres_test my_test;
+    
+//     PetscPrintf(PETSC_COMM_WORLD,"GMRES Algorithm\n");
+    details::benchmark_result<cycles_t> dv;
+    details::perform_benchmark(my_test,1.0, dv);
+    PetscPrintf(PETSC_COMM_WORLD,"%D %Fe+06\n",nprocs,dv.median/1e6);
+
+    
+    KSP &ksp(my_test.ksp); 
+    Vec &u(my_test.x);
+    Vec &b(my_test.b);
+    Mat &A(my_test.A); 
+    
+  /*
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
+  
+    KSPDestroy(&ksp); 
+    VecDestroy(&u);      
+    VecDestroy(&b);     
+    MatDestroy(&A); 
+
+  /*
+     Always call PetscFinalize() before exiting a program.
+  */
+
+    PetscFinalize();
+    
+  return 0;
+}
---
+++ Petsc Code/petsc_gmres_test.h
@@ -0,0 +1,114 @@
+#ifndef PETSC_GMRES_TEST_H
+#define PETSC_GMRES_TEST_H
+
+#include "petscksp.h"
+
+class petsc_gmres_test
+{
+public:
+  Vec            x0,x,b,u;  /* approx solution, RHS, exact solution */
+  Mat            A;        /* linear system matrix */
+  KSP            ksp;     /* linear solver context */
+  PC		 prec;
+  PetscReal      norm;     /* norm of solution error */
+  PetscReal      eps;    
+  PetscInt       i,j,k,Istart,Iend,N,m,n,its,maxits;
+  PetscScalar    v;  
+    
+  petsc_gmres_test():eps(0.05),N(10),maxits(15)
+  {
+   
+    PetscOptionsGetInt(PETSC_NULL,"-N",&N,PETSC_NULL);
+    PetscOptionsGetInt(PETSC_NULL,"-maxits",&maxits,PETSC_NULL);
+    
+    m=N; n=N;
+   
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	  Compute the matrix and right-hand-side vector that define
+	  the linear system, Ax = b.
+      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    
+    MatCreate(PETSC_COMM_WORLD,&A);
+    MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,m,n);
+    MatSetUp(A);
+    
+    MatSetFromOptions(A);
+    
+//     MatMPIDenseSetPreallocation(A,PETSC_NULL);
+    MatGetOwnershipRange(A,&Istart,&Iend);     
+
+    for (i=Istart; i<Iend; i++) { 
+      v = 2.1;
+      MatSetValues(A,1,&i,1,&i,&v,INSERT_VALUES);
+      
+      k=1;
+      if (i-k>=0) { j=i-k; v = -1.0-k*eps; MatSetValues(A,1,&i,1,&j,&v,INSERT_VALUES); }
+      if (i+k<=N-1) { j=i+k; v = -1.0+k*eps; MatSetValues(A,1,&i,1,&j,&v,INSERT_VALUES); }
+    }
+
+    MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY); 
+    MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);
+    
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+			      Create parallel vectors.
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+    VecCreate(PETSC_COMM_WORLD,&u); 
+    VecSetSizes(u,PETSC_DECIDE,m); 
+    VecSetFromOptions(u); 
+    VecDuplicate(u,&b);  
+    VecDuplicate(b,&x);
+    VecDuplicate(x,&x0);
+    
+    /* First element of x0 = 1 */ 
+    v=1.0; i=0; VecSetValues(x0,1,&i,&v,INSERT_VALUES);
+    VecAssemblyBegin(x0);
+    VecAssemblyEnd(x0);
+    VecCopy(x0,x);
+    
+    VecSet(u,1.0);     
+    MatMult(A,u,b); 
+
+    /*
+      View the exact solution vector if desired
+    */
+    
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+		  Create the linear solver and set various options
+      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    /* 
+      Create linear solver context
+    */
+    KSPCreate(PETSC_COMM_WORLD,&ksp);
+
+    /* 
+      Set operators. Here the matrix that defines the linear system
+      also serves as the preconditioning matrix.
+    */
+    
+    KSPGetPC(ksp,&prec);
+    PCSetType(prec,PCNONE);
+  
+    KSPSetOperators(ksp,A,A,SAME_PRECONDITIONER);
+    KSPSetNormType(ksp,KSP_NORM_UNPRECONDITIONED);
+
+    KSPSetType(ksp,KSPGMRES);
+    KSPGMRESSetOrthogonalization(ksp,KSPGMRESClassicalGramSchmidtOrthogonalization);
+    KSPSetInitialGuessNonzero(ksp,PETSC_TRUE);
+
+    KSPSetTolerances(ksp,1.e-6,PETSC_DEFAULT,PETSC_DEFAULT,maxits); 
+    
+    KSPSetFromOptions(ksp);
+
+  };
+  
+  void operator()()
+  {
+    VecCopy(x0,x);
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+		    Solve the linear system
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    KSPSolve(ksp,b,x);
+  };
+};
+
+#endif // PETSC_GEMRES_TEST_H
